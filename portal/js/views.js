9da38e8c49537d4945e23517ef5afb432f925229
WandWeb2 <245160472+WandWeb2@users.noreply.github.com>
2026-04-30 20:54:47 +0000

    🧹 [Code Health] Remove leftover console.log from StandaloneDebugPanel

--- portal/js/views.js
+++ portal/js/views.js
@@ -2802,7 +2802,6 @@ window.StandaloneDebugPanel = ({ token }) => {
                 body: JSON.stringify({ action: 'get_system_logs', token })
             });
             const responseText = await response.text();
-            console.log('[StandaloneDebug] Raw response:', responseText);
             const data = JSON.parse(responseText);
             console.log('[StandaloneDebug] Logs loaded:', data);
             
